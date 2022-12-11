d6e0173a6e1cefced0f10022440b5daac3faf893
Sammy-CK <sammycherono2696@gmail.com>
2022-12-11 19:35:11 +0300

    Updated functions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { moviesLoaded() })
+
+
+  //fetches all movie details upon page load
+  function moviesLoaded(){
 
     const listMovies = document.getElementById('films')
-
     const movieContainer = document.getElementById('movieContainer')
-
     const posterDiv = document.getElementById('posterDiv')
 
-showFirstMovie()
-
-    function showFirstMovie() {
-    fetch('http://localhost:3000/films/1')
-    .then(resp => resp.json())
-    .then(data => createMovieDetailCard(data, movieContainer))
-    }
+showMovie(1, movieContainer)
 
     fetch('http://localhost:3000/films')
     .then(resp => resp.json())
@@ -20,54 +16,32 @@ showFirstMovie()
         
         data.forEach(movie => {
             const liMovie = document.createElement('li')
+            liMovie.style.cursor = 'pointer'
 
             liMovie.addEventListener('click', () => {
                 movieContainer.innerHTML = ''
-                fetch(`http://localhost:3000/films/${movie.id}`)
-                .then(resp => resp.json())
-                .then(data => {
-                    createMovieDetailCard(data, movieContainer)
-                })
-
-
+               showMovie(movie.id, movieContainer)
             })
 
             const pTitle = document.createElement('p')
             const deleteBtn = document.createElement('button')
             deleteBtn.innerText = 'DELETE'
-            deleteBtn.addEventListener('click',() => {
-              fetch(`http://localhost:3000/films/${movie.id}`,{
-                method : "DELETE",
-                headers : {
-                    "Content-Type" : "application/json",
-                    Accept : "application/json"
-                }
-              })
-              .then(resp => resp.json())
-              .then(data => {
-                liMovie.remove();
 
-              })
-                
+            deleteBtn.addEventListener('click',() => { 
+                deleteMovie(movie.id, liMovie)
             })
+
             pTitle.innerText = movie.title
             liMovie.appendChild(pTitle)
             pTitle.appendChild(deleteBtn)
             listMovies.appendChild(liMovie)
         })
-
     })
+  }
 
-
-    
-})
-
-/**
-* createMovieDetailsCard : Creates a movie detail card for movie.
-*   
-*   @data: fetch request response for individual movie
-**/
+//createMovieDetailsCard : Creates a movie detail card from data using fetch request response for individual movie
 function createMovieDetailCard(data, tagToAppend) {
+
     const poster = document.createElement('img')
         poster.src = data.poster
 
@@ -86,7 +60,7 @@ function createMovieDetailCard(data, tagToAppend) {
         <p><b>Available Tickets</b></p>`
         tagToAppend.appendChild(showtime)
 
-
+//Calculate available movie tickets
         let capacity = data.capacity
         let titcketsSold = data.tickets_sold
         let remainingTickets = capacity - titcketsSold
@@ -103,58 +77,67 @@ function createMovieDetailCard(data, tagToAppend) {
         const buyTicketBtn = document.createElement('button')
         buyTicketBtn.innerText = 'BUY TICKET'
 
-        if(remainingTickets >= 1){
-                
-            availableTickets.innerText =  remainingTickets
-         }else{
-             availableTickets.innerText = 0
-             buyTicketBtn.disabled = true
-             buyTicketBtn.innerText = 'SOLD OUT'
-             buyTicketBtn.style.backgroundColor = 'grey'
-         }
 
+updateBuyBtn (remainingTickets, availableTickets, buyTicketBtn)
 
+//update DOM and json server on available tickets upon buying
         buyTicketBtn.addEventListener('click',() => {
-//start
-fetch(`http://localhost:3000/films/${data.id}`)
-                .then(resp => resp.json())
-                .then(datam => {
-
-
-fetch(`http://localhost:3000/films/${data.id}`,{
+            fetch(`http://localhost:3000/films/${data.id}`)
+            .then(resp => resp.json())
+            .then(datam => {
+            
+            fetch(`http://localhost:3000/films/${data.id}`,{
             method : "PATCH",
             headers : {
             "Content-Type" : "application/json",
             Accept : "application/json"
-        },
-        body : JSON.stringify({
+            },
+            body : JSON.stringify({
             tickets_sold : ((datam.tickets_sold < datam.capacity)?datam.tickets_sold + 1: datam.capacity)
-        })
-        })
-.then(resp => resp.json())
-.then(dataz => {
-    remainingTickets--
-
-   if(remainingTickets >= 1){
-                
-               availableTickets.innerText =  remainingTickets
-            }else{
-                availableTickets.innerText = 0
-                buyTicketBtn.disabled = true
-                buyTicketBtn.innerText = 'SOLD OUT'
-                buyTicketBtn.style.backgroundColor = 'grey'
-            }
-
-
-})
-})
-//end
-
-         
-
+            })
+            })
+            .then(resp => resp.json())
+            .then(dataz => {
+            remainingTickets--
             
-
+            updateBuyBtn (remainingTickets, availableTickets, buyTicketBtn)
+            
+            })
+            })
         })
         tagToAppend.appendChild(buyTicketBtn)
-
 }
+
+
+//shows movie details on the DOM
+function showMovie(id, movieContainer) {
+    fetch(`http://localhost:3000/films/${id}`)
+    .then(resp => resp.json())
+    .then(data => createMovieDetailCard(data, movieContainer))
+    }
+
+
+//deletes a movie from the ul menu list and the server
+function deleteMovie (movieID, liMovie)  {
+    fetch(`http://localhost:3000/films/${movieID}`,{
+      method : "DELETE",
+      headers : {
+          "Content-Type" : "application/json",
+          Accept : "application/json"
+      }
+    })
+    .then(() => { liMovie.remove(); })
+  }
+
+//disable button on sold out tickets
+  function updateBuyBtn (remainingTickets, availableTickets, buyTicketBtn){
+    if(remainingTickets >= 1){                
+        availableTickets.innerText =  remainingTickets
+     }else{
+         availableTickets.innerText = 0
+         buyTicketBtn.disabled = true
+         buyTicketBtn.innerText = 'SOLD OUT'
+         buyTicketBtn.style.backgroundColor = 'grey'
+         buyTicketBtn.style.cursor = 'not-allowed'
+     }
+  }
