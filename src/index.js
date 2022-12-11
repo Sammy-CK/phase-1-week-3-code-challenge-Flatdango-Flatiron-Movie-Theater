d0f2589af4aa8f6d61d209875fea9f98474ba358
Sammy-CK <sammycherono2696@gmail.com>
2022-12-11 12:15:17 +0300

    Initial completion

---
+++ src/index.js
@@ -0,0 +1,91 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const listMovies = document.getElementById('films')
+
+    const movieContainer = document.getElementById('movieContainer')
+
+    const posterDiv = document.getElementById('posterDiv')
+
+    fetch('http://localhost:3000/films/1')
+    .then(resp => resp.json())
+    .then(data => createMovieDetailCard(data, movieContainer))
+
+
+    fetch('http://localhost:3000/films')
+    .then(resp => resp.json())
+    .then(data => {
+        
+        data.forEach(movie => {
+            const liMovie = document.createElement('li')
+
+            const pTitle = document.createElement('p')
+            pTitle.innerText = movie.title
+            liMovie.appendChild(pTitle)
+            listMovies.appendChild(liMovie)
+        })
+
+    })
+
+
+    
+})
+
+/**
+* createMovieDetailsCard : Creates a movie detail card for movie.
+*   
+*   @data: fetch request response for individual movie
+**/
+function createMovieDetailCard(data, tagToAppend) {
+    const poster = document.createElement('img')
+        poster.src = data.poster
+
+        const posterDiv = document.getElementById('posterDiv')
+
+        posterDiv.appendChild(poster)
+        tagToAppend.appendChild(posterDiv)
+
+        const title = document.createElement('h2')
+        title.innerText = data.title
+        tagToAppend.appendChild(title)
+
+        const runtime = document.createElement( 'p')
+        runtime.innerHTML = `<b>Run time:</b> ${data.runtime}`
+        tagToAppend.appendChild(runtime)
+
+        const showtime = document.createElement( 'p')
+        showtime.innerHTML = `<b>Show time:</b> ${data.showtime}
+        <p><b>Available Tickets</b></p>`
+        tagToAppend.appendChild(showtime)
+
+
+        const capacity = data.capacity
+        const titcketsSold = data.tickets_sold
+        let remainingTickets = capacity - titcketsSold
+
+        const availableTickets = document.createElement('p')
+        availableTickets.innerText =  remainingTickets
+        tagToAppend.appendChild(availableTickets)
+
+        const description = document.createElement( 'p')
+        description.innerText = data.description
+        tagToAppend.appendChild(description)
+
+
+        const buyTicketBtn = document.createElement('button')
+        buyTicketBtn.innerText = 'BUY TICKET'
+        buyTicketBtn.addEventListener('click',() => {
+            if(remainingTickets > 1){
+                remainingTickets--
+               availableTickets.innerText =  remainingTickets
+            }else{
+                availableTickets.innerText = 0
+                buyTicketBtn.disabled = true
+                buyTicketBtn.innerText = 'SOLD OUT'
+            }
+
+            
+
+        })
+        tagToAppend.appendChild(buyTicketBtn)
+
+}
